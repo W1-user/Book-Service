f13be40ed9949f5755411ed63b351f2d1763065e
W1-user <wolframgit@gmail.com>
2026-04-26 13:06:00 +0300

    Refactoring main.py

--- src/book_service/main.py
+++ src/book_service/main.py
@@ -1,8 +1,10 @@
 from contextlib import asynccontextmanager
 from fastapi import FastAPI
+import uvicorn
 
+from book_service.config import settings
 from book_service.api.v1.auth_router import router as auth_router
-from book_service.api.v1.book_router import router as book_router
+# from book_service.api.v1.book_router import router as book_router
 
 from book_service.database import Base, engine
 
@@ -14,5 +16,8 @@ async def lifespan(app: FastAPI):
     await engine.dispose()
 
 app = FastAPI(title="Book Service", lifespan=lifespan)
-app.include_router(auth_router)
-app.include_router(book_router)
+app.include_router(auth_router, prefix=settings.default_prefix)
+# app.include_router(book_router)
+
+if __name__ == "__main__":
+    uvicorn.run(app="main:app", reload=True)
