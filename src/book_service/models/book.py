5986df665b7c83cfbd3531b2ef379f943a0074e3
W1-user <wolframgit@gmail.com>
2026-04-30 16:12:48 +0300

    Refactoring book database

--- src/book_service/models/book.py
+++ src/book_service/models/book.py
@@ -1,4 +1,4 @@
-from sqlalchemy import String, Integer, Float, Text
+from sqlalchemy import String, Integer, Float, Text, Boolean
 from sqlalchemy.orm import Mapped, mapped_column
 
 from book_service.database import Base
@@ -16,6 +16,8 @@ class Book(Base):
     binding: Mapped[str] = mapped_column(String, nullable=True)
     publisher: Mapped[str] = mapped_column(String, nullable=True)
     year: Mapped[int] = mapped_column(Integer, nullable=True)
+
+    is_popular: Mapped[bool] = mapped_column(Boolean, default=False)
 
     def __repr__(self) -> str:
         return f"Book - (book_id='{self.book_id}', title='{self.title}', author='{self.author}')"
